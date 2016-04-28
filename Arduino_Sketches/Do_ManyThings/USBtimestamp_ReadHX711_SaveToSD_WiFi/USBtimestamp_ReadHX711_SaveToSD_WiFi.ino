b8e61f21bcb828a956a40d4954283019657e905a
Brendan Wayne <brendan.t29@gmail.com>
2016-04-28 15:31:43 -0500

    Somewhat broken

--- Arduino_Sketches/Do_ManyThings/USBtimestamp_ReadHX711_SaveToSD_WiFi/USBtimestamp_ReadHX711_SaveToSD_WiFi.ino
+++ Arduino_Sketches/Do_ManyThings/USBtimestamp_ReadHX711_SaveToSD_WiFi/USBtimestamp_ReadHX711_SaveToSD_WiFi.ino
@@ -7,16 +7,13 @@
 #include <TimeLib.h>
 #include <HX711.h>
 
-const int chipPin = 4;
-#define TIME_HEADER  'T'   // Header tag for serial time sync message
-#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
-
 //===============Change values as necessary===================
-const String stationName = "Northwest";
+const String stationName = "Test Station";
 const String fileName = "DATALOG.txt";
 #define SCALE_COUNT 2
 #define TIME_BETWEEN_READINGS 50 //time between readings, in milliseconds
 #define TIME_BETWEEN_SAVES 500 //time between saves, in milliseconds
+#define TIME_BETWEEN_UPLOADS 10000
 
 int pinsDOUT[SCALE_COUNT] = {7,A0}; 
 //The pins hooked up to the respective cells' DOUT
@@ -31,17 +28,48 @@ int gain = 128;
 //the gain can be 128 or 64 on channel A, or 32 on channel B
 //=============================================================
 
+const int chipPin = 4;
+#define TIME_HEADER  'T'   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
+
 HX711 *allCells[SCALE_COUNT] = {NULL, NULL}; 
 
 unsigned long prevRead = 0;
 unsigned long prevSave = 0;
+unsigned long prevUpload = 0;
 float cellReadings[SCALE_COUNT] = {0,0};
 int readsSinceSave = 0;
 File dataFile;
 
-void setup()  {
+// These are the interrupt and control pins
+#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
+// These can be any two pins
+#define ADAFRUIT_CC3000_VBAT  5
+#define ADAFRUIT_CC3000_CS    10
+// Use hardware SPI for the remaining pins
+// On an UNO, SCK = 13, MISO = 12, and MOSI = 11
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
+                                         SPI_CLOCK_DIVIDER); // you can change this clock speed
+
+#define WLAN_SSID       "The Squishy of Rick"           // cannot be longer than 32 characters!
+#define WLAN_PASS       "brendane"
+// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
+#define WLAN_SECURITY   WLAN_SEC_WPA2
+
+#define IDLE_TIMEOUT_MS  3000      // Amount of time to wait (in milliseconds) with no data 
+                                   // received before closing the connection.  If you know the server
+                                   // you're accessing is quick to respond, you can reduce this value.
+
+// What page to post to!
+#define WEBSITE      "winter-runoff.soils.wisc.edu"
+
+uint32_t ip;
+Adafruit_CC3000_Client client;
+
+//=====================================================================
+
+void setup() {
   Serial.begin(9600);
-  //pinMode(13, OUTPUT);
   setSyncProvider( requestSync);  //set function to call when sync required
   
   dataFile = startSDfile(chipPin, fileName);
@@ -53,14 +81,22 @@ void setup()  {
     allCells[ii]->tare();
     allCells[ii]->set_scale(calibrations[ii]);
   }
-  delay(200);
+  
+  if (!cc3000.begin())
+  {
+    Serial.println(F("Couldn't begin()! Check your wiring?"));
+    while(1);
+  }
+  
+  
 }
 
 void loop(){    
   if (Serial.available()) {
     processSyncMessage();
   }
-  
+
+  //Read each loadcell
   if (millis() - prevRead > TIME_BETWEEN_READINGS) {
     for (int ii=0; ii<SCALE_COUNT; ii++) {
       cellReadings[ii] += allCells[ii]->get_units();
@@ -68,11 +104,17 @@ void loop(){
     readsSinceSave++;
     prevRead = millis();
   }
-  
+
+  //average the readings and save to SD card
   if (millis() - prevSave > TIME_BETWEEN_SAVES) {
     
     saveString(makeDataString(cellReadings, &readsSinceSave, stationName), dataFile);
     prevSave = millis();
+  }
+
+  //Upload to web server
+  if (millis() - prevUpload > TIME_BETWEEN_UPLOADS) {
+    
   }
   
 }
@@ -114,7 +156,7 @@ String makeDataString(float *cellReadings,int *readsSinceSave,String stationName
   dataString += dateDisplay(t);
   dataString += " ";
   dataString += timeDisplay(t);
-  dataString += ",\""+stationName+"\""; // insert real station name here
+  dataString += ",\'"+stationName+"\'"; // insert real station name here
   for (int ii=0; ii<SCALE_COUNT; ii++) {
     dataString += ",";
     dataString += cellReadings[ii] / *readsSinceSave;
@@ -129,7 +171,23 @@ void saveString(String mystring, File myFile){
   myFile.println(mystring);
   myFile.flush();
 }
-
+/*
+bool uploadStrings(String mystrings[]) {
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+    return false;
+  }
+  while (!cc3000.checkDHCP())
+  {
+    delay(100); // ToDo: Insert a DHCP timeout!
+  }
+  
+  for (int ii = 0; ii < 4; ii++) {
+    if (!client.connected()) {
+      client = connectToServer();
+   }
+  }
+}
+*/
 void postString(String data,Adafruit_CC3000_Client client) {
   data = "csv_line="+data;
   char datachars[data.length()+1];
@@ -137,13 +195,19 @@ void postString(String data,Adafruit_CC3000_Client client) {
   if (client.connected()) { 
     client.fastrprintln(F("POST /time_series_data/upload HTTP/1.1")); 
     client.fastrprint(F("Host: ")); 
-    //client.fastrprintln(WEBSITE);// SERVER ADDRESS HERE TOO
+    client.fastrprintln(WEBSITE);// SERVER ADDRESS HERE TOO
     client.fastrprintln(F("Content-Type: application/x-www-form-urlencoded")); 
     client.fastrprint(F("Content-Length: ")); 
     client.println(data.length()); 
     client.println(); 
     client.fastrprint(datachars); 
   } 
+}
+
+Adafruit_CC3000_Client connectToServer() {
+  cc3000.getHostByName(WEBSITE, &ip);
+  Adafruit_CC3000_Client client = cc3000.connectTCP(ip, 80);
+  return client;
 }
 
 File startSDfile(int chipSelect, String filePath) {
@@ -171,4 +235,5 @@ time_t requestSync()
   //Serial.write(TIME_REQUEST);  
   return 0; // the time will be sent later in response to serial mesg
 }
+
 
