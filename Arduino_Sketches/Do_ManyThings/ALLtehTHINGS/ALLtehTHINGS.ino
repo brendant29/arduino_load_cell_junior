bade7161655e567fe8f77ffbd3854a74ba185ef9
Brendan Wayne <brendan.t29@gmail.com>
2016-08-23 10:47:20 -0500

    Implemented RTC based sleep and wake, and added more comments

--- Arduino_Sketches/Do_ManyThings/ALLtehTHINGS/ALLtehTHINGS.ino
+++ Arduino_Sketches/Do_ManyThings/ALLtehTHINGS/ALLtehTHINGS.ino
@@ -1,6 +1,5 @@
 #include <avr/sleep.h>
 #include <avr/power.h>
-#include <avr/wdt.h>
 
 #include <Wire.h>
 #include <DS3232RTC.h>
@@ -19,10 +18,11 @@ const String stationName = "Test Station";
 String serialCompleteName = "DATALOG.txt";
 String uploadBufName = "UPLOAD.txt";
 String errorBufName = "ERROR.txt";
-#define SCALE_COUNT 4
+#define SCALE_COUNT 4  //how many loadcells
 #define TIME_BETWEEN_READINGS .5 //time between readings, in seconds
-#define TIME_BETWEEN_SAVES 2 //time between saves, in seconds
-#define TIME_BETWEEN_UPLOADS 10
+#define TIME_BETWEEN_SAVES 2     //time between saves, in seconds
+#define TIME_BETWEEN_UPLOADS 10  //time between uploads, in seconds
+#define RTC_ALARM_PIN 18 //Must be one of: 2, 18, 19
 
 #define DEBUG 0 //whether or not to do things over the serial port
 
@@ -52,7 +52,7 @@ int gain = 128;
 #endif
 
 #define CHIP_PIN 4
-#define TIME_HEADER  "OK::"   // Header tag for serial time sync message
+#define TIME_HEADER  "OK::"   // Header tag for time sync message
 
 unsigned long prevRead = 0;
 unsigned long prevSave = 0;
@@ -61,21 +61,22 @@ float cellReadings[SCALE_COUNT];
 int readsSinceSave = 0;
 
 
-// These are the interrupt and control pins
-#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
+// These are the interrupt and control pins 
+//NOTE: with a shield, pins can't be changed
+#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin! 3 on shield
 // These can be any two pins
-#define ADAFRUIT_CC3000_VBAT  5
-#define ADAFRUIT_CC3000_CS    10
+#define ADAFRUIT_CC3000_VBAT  5  //5 on shield
+#define ADAFRUIT_CC3000_CS    10 //10 on shield
 // Use hardware SPI for the remaining pins
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
 
 //The name and password of the Wifi acces point:
-#define WLAN_SSID       "GALAXY_S4_4545"  // cannot be longer than 32 characters!
-#define WLAN_PASS       "wljo2151"
+#define WLAN_SSID       "GALAXY_S4_4545"  //name of wifi. cannot be longer than 32 characters!
+#define WLAN_PASS       "wljo2151"        //password for wifi
 
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
-#define DHCP_TIMEOUT 5
+#define DHCP_TIMEOUT 5  //time, in seconds, before it gives up checking DHCP
 #define IDLE_TIMEOUT_MS  3000      // Amount of time to wait (in milliseconds) with no data 
                                    // received before closing the connection.  If you know the server
                                    // you're accessing is quick to respond, you can reduce this value.
@@ -96,9 +97,9 @@ void setup() {
     while(!Serial.available());
     Serial.readString();
   #endif
-  if (SD.begin(CHIP_PIN)) sdBegun = true;
+  if (SD.begin(CHIP_PIN)) sdBegun = true; 
   
-  //setting up the cells
+  //setting up the loadcells
   for(int ii=0; ii<SCALE_COUNT; ii++){
     allCells[ii] = new HX711(pinsDOUT[ii], pinsSCK[ii]);
     allCells[ii]->set_gain(gain);
@@ -107,7 +108,19 @@ void setup() {
     allCells[ii]->power_down();
     cellReadings[ii] = 0;
   }
-  setTime(RTC.get());
+
+  setTime(RTC.get()); //set arduino internal time from RTC
+
+  pinMode(RTC_ALARM_PIN, INPUT_PULLUP); 
+  //INPUT mode would likely work, but INPUT_PULLUP means it won't go LOW due to random noise.
+  
+  RTC.alarmInterrupt(2, false); //disable alarm 2
+  RTC.alarmInterrupt(1, true);   //enable alarm 1
+  RTC.setAlarm(ALM1_EVERY_SECOND, 0, 0, 0, 0); //set alarm 1 to fire once per second
+  
+  RTC.alarm(1);  //clear both alarms if they've gone off
+  RTC.alarm(2);
+  
   prevSave = now();
   prevUpload = now();
   DEBUG_PRINTLN(F("Setup complete."));
@@ -140,16 +153,21 @@ void loop(){
   if (now() - prevUpload > TIME_BETWEEN_UPLOADS) {
     uploadFromFile(uploadBufName, errorBufName);
     prevUpload = now();
+    /*Uploads tend to take a while, so for testing we set prevUpload afterwards,
+    * so it doesn't start another upload immediatly. This means that it will 
+    * actually upload every (TIME_BETWEEN_UPLOADS + (time spent on upload)) 
+    * seconds. For more accurate timing, set prevUpload *before* uploading.
+    */
   }
   delay(100);
-  sleep();
+  goToSleep();
 }
 
 //=============================================
 //==================FUNCTIONS==================
 //=============================================
 
-//Given seconds since epoch, return the current date
+//Given seconds since epoch, return the current date (yyyy-mm-dd)
 String dateDisplay(time_t t) {
   String date = String(year(t));
   date += "-";
@@ -159,7 +177,7 @@ String dateDisplay(time_t t) {
   return date;
 }
 
-//Given seconds since epoch, return the current time
+//Given seconds since epoch, return the current time (hh:mm:ss)
 String timeDisplay(time_t t) {
   String timer = stringDigits(hour(t));
   timer += ":";
@@ -179,14 +197,14 @@ String stringDigits(int digits){
   return strDigits;
 }
 
-//Assembles a datum for recording
+//Assembles a datum for recording (yyyy-mm-dd hh:mm:ss,'stationName',lc1,lc2,...,lcn)
 String makeDataString(float *cellReadings, int *readsSinceSave, String stationName, String *myString) {
   *myString = "";
   time_t t = now();
   *myString += dateDisplay(t);
   *myString += " ";
   *myString += timeDisplay(t);
-  *myString += ",\'"+stationName+"\'"; // insert real station name here
+  *myString += ",\'"+stationName+"\'"; 
   for (int ii=0; ii<SCALE_COUNT; ii++) {
     *myString += ",";
     *myString += cellReadings[ii] / *readsSinceSave;
@@ -231,7 +249,7 @@ bool uploadFromFile(String uploadName, String errorName) {
   }
   
   if (!sdBegun) {
-    DEBUG_PRINTLN("I don't think the card's available.");
+    DEBUG_PRINTLN("card not available!");
     client.close();
     cc3000.disconnect();
     delay(1000);
@@ -250,23 +268,23 @@ bool uploadFromFile(String uploadName, String errorName) {
     if (!postString(dataString, client)) {
       saveToSD(dataString, errorName);
       #if DEBUG
-        saveToSD(dataString, "ERRLOG.TXT");
+        saveToSD(dataString, "ERRLOG.TXT"); //keep a log of failed uploads
       #endif
     }
     #if DEBUG
-      saveToSD(dataString, "REUPLOG.TXT");
+      saveToSD(dataString, "UPLOG.TXT"); //keep a log of all attempted uploads
     #endif
   }
 
   uploadFile.close();
+  client.close();
+  cc3000.disconnect();
 
   DEBUG_PRINT("Juggling Files...");
   
-  SD.remove(uploadName);
+  SD.remove(uploadName); //get rid of uploaded file
   
-  client.close();
-  cc3000.disconnect();
-  
+  //copy contents of error file into new upload file
   uploadFile = SD.open(uploadName, FILE_WRITE);
   File errorFile = SD.open(errorName, FILE_READ);
   char buf[100];
@@ -279,7 +297,10 @@ bool uploadFromFile(String uploadName, String errorName) {
   }
   uploadFile.close();
   errorFile.close();
-  SD.remove(errorName);
+  
+  
+  SD.remove(errorName); //get rid of error file
+  
   DEBUG_PRINTLN("Files juggled.");
   
   delay(1000);
@@ -287,26 +308,28 @@ bool uploadFromFile(String uploadName, String errorName) {
   return true;
 }
 
-bool postString(String data,Adafruit_CC3000_Client client) {
+bool postString(String data, Adafruit_CC3000_Client client) {
   String PostData = "csv_line=" + data;
-  if (client.connected()) {
+  if (client.connected()) {  //if we're not connected to the website, don't bother.
     DEBUG_PRINT(F("Posting..."));
 
     //can use print instead of fastrprint 
     //if program space is at a premium
     
-    client.fastrprintln(F("POST /time_series_data/upload HTTP/1.1"));
-    client.fastrprint(F("Host: "));
-    client.fastrprintln(F(WEBSITE));
-    client.fastrprintln(F("User-Agent: Arduino/1.0"));
-    client.fastrprintln(F("Connection: close"));
-    client.fastrprint(F("Content-Length: "));
+    client.fastrprintln("POST /time_series_data/upload HTTP/1.1");
+    client.fastrprint("Host: ");
+    client.fastrprintln(WEBSITE);
+    client.fastrprintln("User-Agent: Arduino/1.0");
+    client.fastrprintln("Connection: close");
+    client.fastrprint("Content-Length: ");
     client.println(PostData.length());
     client.println();
     client.println(PostData);
-    DEBUG_PRINTLN(F("posted!"));
+    DEBUG_PRINTLN("posted!");
   }
   delay(1000); //time enough to finish upload
+
+  //if we don't get a specific response from the server, return false
   if (!client.available()) {
     return false;
   }
@@ -325,13 +348,8 @@ bool postString(String data,Adafruit_CC3000_Client client) {
       Serial.print(c);
       lastRead = millis();
     }
-  }*//*
-  Serial.println(data);
-  Serial.println("Does this \'upload\' succeed?");
-  while (!Serial.available());
-  if (!Serial.parseInt()) {
-    return false;
   }*/
+  
   return true;
 }
 
@@ -353,7 +371,7 @@ bool processSyncMessage(Adafruit_CC3000_Client client) {
   if(client.find(TIME_HEADER)) {
     servertime = client.parseInt();
     if( servertime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
-      //setTime(servertime); // Sync Arduino clock to the time received on the serial port
+      //setTime(servertime); // Sync Arduino clock to the time received from server
     }
     return true;
   }
@@ -372,22 +390,22 @@ bool saveToSD(String myString, String filePath) {
   return true;
 }
 
-void sleep()
-{
-  // Set sleep to full power down.  Only external interrupts or 
-  // the watchdog timer can wake the CPU!
+void goToSleep() {
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  RTC.alarm(1); //clear alarm 1
+  delay(100);
+  sleep_enable();
+  attachInterrupt(digitalPinToInterrupt(RTC_ALARM_PIN), wake, LOW);
+  delay(100);
+  sleep_cpu();
 
-  // Turn off the ADC while asleep.
-  power_adc_disable();
-
-  // Enable sleep and enter sleep mode.
-  sleep_mode();
-
-  // CPU is now asleep and program execution completely halts!
-  // Once awake, execution will resume at this point.
   
-  // When awake, disable sleep mode and turn on all devices.
-  sleep_disable();
-  power_all_enable();
+  sleep_disable(); //probably redundant; feels safer
+  delay(100);
 }
+
+void wake() {
+  detachInterrupt(5);
+  sleep_disable();
+}
+
